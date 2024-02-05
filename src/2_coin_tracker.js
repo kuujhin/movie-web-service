e15352b80813ceaf73cd7fbbdf88e4e65b0ef7f6
kuujhin <kdoubleu0711@gmail.com>
2024-02-05 20:33:19 +0900

    coin tracker

---
+++ src/2_coin_tracker.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function CoinTracker() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=20")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CoinTracker;
