6dbe9b7bf060ba2d0c6f240051a82e3bcb55bb17
kuujhin <kdoubleu0711@gmail.com>
2024-02-05 23:00:50 +0900

    style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,23 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, year, coverImg, title, summary, genres }) {
   return (
-    <div key={id}>
-      <img src={coverImg} alt="title" />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-
-      {summary.length < 200 ? (
-        <p>{summary}</p>
-      ) : (
-        <p>{summary.split(" ").slice(0, 30).join(" ")}... </p>
-      )}
-
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -19,16 +20,17 @@ function Home() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>The movies! {loading ? "" : `(${movies.length})`}</h1>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
