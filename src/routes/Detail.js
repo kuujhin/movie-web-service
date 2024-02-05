3ee694e8ad98cf00cd8b1cb48a50b3a8e57a7df8
kuujhin <kdoubleu0711@gmail.com>
2024-02-05 22:35:52 +0900

    detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getDetails = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+      setLoading(false);
+    };
+    getDetails();
+  }, [id]);
+  console.log(movie);
+
+  return (
+    <div>
+      <h1>{id}</h1>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt="title" />
+          <h2>{movie.title}</h2>
+          <p>{movie.description_full}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
