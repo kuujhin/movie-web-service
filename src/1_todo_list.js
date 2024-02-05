2ddf943030c7b5bbac98d1a5a8fb0c2b62ae64f6
kuujhin <kdoubleu0711@gmail.com>
2024-02-05 20:33:13 +0900

    todo list

---
+++ src/1_todo_list.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function TodoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    // [6, ...[1, 2, 3, 4]] === [6, 1, 2, 3, 4];
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    // state를 직접 수정하지 못함. 함수 사용
+    // toDo = "" // 에러
+    setToDo("");
+  };
+
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your todo..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
